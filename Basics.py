9aa1e6286228b0b8e201befa98a3a601aa6f7bc8
Sahib <72847553+Sompus@users.noreply.github.com>
2021-08-02 12:43:01 +0400

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -13,28 +13,20 @@ with mp_hands.Hands (min_detection_confidence = 0.8, min_tracking_confidence = 0
     while cap.isOpened () :
         ret, frame = cap.read ()
 
-        # BGR 2 RGB
         image = cv2.cvtColor (frame, cv2.COLOR_BGR2RGB)
 
-        # Flip on horizontal
         image = cv2.flip (image, 1)
 
-        # Set flag
         image.flags.writeable = False
-
-        # Detections
+        
         results = hands.process (image)
 
-        # Set flag to true
         image.flags.writeable = True
 
-        # RGB 2 BGR
         image = cv2.cvtColor (image, cv2.COLOR_RGB2BGR)
 
-        # Detections
         print (results)
 
-        # Rendering results
         if results.multi_hand_landmarks :
             for num, hand in enumerate (results.multi_hand_landmarks) :
                 mp_drawing.draw_landmarks (image, hand, mp_hands.HAND_CONNECTIONS,
@@ -79,9 +71,9 @@ with mp_hands.Hands (min_detection_confidence = 0.8, min_tracking_confidence = 0
         if results.multi_hand_landmarks :
             for num, hand in enumerate (results.multi_hand_landmarks) :
                 mp_drawing.draw_landmarks (image, hand, mp_hands.HAND_CONNECTIONS,
-                                           mp_drawing.DrawingSpec (color = (121, 22, 76), thickness = 2,
-                                                                   circle_radius = 4),
-                                           mp_drawing.DrawingSpec (color = (250, 44, 250), thickness = 2,
+                                           mp_drawing.DrawingSpec (color = (50, 205, 50), thickness = 2,         #You can change the colors if you want, check for code "https://flaviocopes.com/rgb-color-codes/"
+                                                                   circle_radius = 3),
+                                           mp_drawing.DrawingSpec (color = (0, 206, 209), thickness = 2,
                                                                    circle_radius = 2),
                                            )
 
